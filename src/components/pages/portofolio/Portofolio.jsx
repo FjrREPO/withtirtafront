34cb614ee122c94ccb9f374845890e073622f1a9
FjrREPO <jossfajar21@gmial.com>
2022-08-12 20:19:25 +0700

    first commit

--- src/components/pages/portofolio/Portofolio.jsx
+++ src/components/pages/portofolio/Portofolio.jsx
@@ -1,51 +1,29 @@
 import React, { useState, useEffect } from "react";
 import "./Portofolio.css";
-import axios from "axios";
+import portofolioData from "./PortofolioData";
 
-function Project() {
-
-    //product list
-    const [products, setProducts] = useState([]);
-
-    useEffect(() => {
-        getProducts();
-    }, []);
-
-    const getProducts = async () => {
-        const response = await axios.get("http://localhost:5000/admin/");
-        setProducts(response.data);
-    };
-
-    //Product Filter
-    const [category, setCategory] = useState([]);
+const Project = () => {
 
     const [filter, setFilter] = useState("Video");
-
-    const getUsers = async () => {
-        const { data } = await axios.get("http://localhost:5000/admin/");
-        setFilter(data);
-    };
-
-    useEffect(() => {
-        
-        getUsers();
-
+    const [data, setData] = useState();
+    
+        useEffect(() => {
         if (filter === "Video") {
-        const filteredData = products.filter(
-            (product) => product.category === filter
-        );
-
-        setFilter(filteredData);
+            const filteredData = portofolioData.filter(
+            (item) => item.category === filter
+            );
+    
+            setData(filteredData);
         }
-
+    
         if (filter === "Photo") {
-        const filteredData = products.filter(
-            (product) => product.category === filter
-        );
-
-        setFilter(filteredData);
+            const filteredData = portofolioData.filter(
+            (item) => item.category === filter
+            );
+    
+            setData(filteredData);
         }
-    }, []);
+    }, [filter]);
 
     const active = 'tab__btn__active'
 
@@ -60,7 +38,7 @@ function Project() {
                 className={` ${
                     filter === "Video" ? active : ""
                 } secondary__btn`}
-                onClick={() => setCategory("Video")}
+                onClick={() => setFilter("Video")}
             >
                 Video
             </button>
@@ -68,21 +46,20 @@ function Project() {
                 className={` ${
                     filter === "Photo" ? active : ""
                 } secondary__btn`}
-                onClick={() => setCategory("Photo")}
+                onClick={() => setFilter("Photo")}
             >
                 Photo
             </button>
         </div>
         <div className="container">
             <div className="row">
-                {products.filter(product => product.category === category)
-                .map((product) => (
+                {data?.map((item) => (
                     <>
-                        <div className="col__3 admin__maincol" key={product.id}>
+                        <div className="col__3 admin__maincol" key={item.id}>
                             <div className="project__box pointer relative" data-aos="zoom-in-right">
                                 <div className="project__box__img pointer relative">
                                     <div className="project__img__box">
-                                    <a href={product.name} target='_blank' rel="noreferrer"><img src={product.url} alt="" className="project__img" /></a>
+                                        <a href={item.link} target='_blank' rel="noreferrer"><img src={item.img} alt="" className="project__img"/></a>
                                     </div>
                                     <div className="mask__effect"/>
                                 </div>
---
+++ src/components/pages/portofolio/PortofolioData.js
@@ -0,0 +1,87 @@
+const portofolioData = [
+  {
+    id: "01",
+    img: "/images/portofolio-01.jpg",
+    category: "Photo",
+    link: "https://www.instagram.com/p/CcvH6rsPYxd/?utm_source=ig_web_copy_link"
+  },
+
+  {
+    id: "02",
+    img: "/images/portofolio-02.jpg",
+    category: "Photo",
+    link: "https://www.instagram.com/p/CbDEMOcvU9-/?utm_source=ig_web_copy_link"
+  },
+
+  {
+    id: "03",
+    img: "/images/portofolio-03.jpg",
+    category: "Photo",
+    link: "https://www.instagram.com/p/CeBpizXv0sD/?utm_source=ig_web_copy_link"
+  },
+
+  {
+    id: "04",
+    img: "/images/portofolio-04.jpg",
+    category: "Photo",
+    link: "https://www.instagram.com/p/CcYV6trP0Lj/?utm_source=ig_web_copy_link"
+  },
+
+  {
+    id: "05",
+    img: "/images/portofolio-05.jpg",
+    category: "Photo",
+    link: "https://www.instagram.com/p/CZyCAdpvZCN/?utm_source=ig_web_copy_link"
+  },
+
+  {
+    id: "06",
+    img: "/images/portofolio-06.jpg",
+    category: "Photo",
+    link: "https://www.instagram.com/p/CQFkObrsmvy/?utm_source=ig_web_copy_link"
+  },
+
+  {
+    id: "07",
+    img: "/images/portofolio-07.jpg",
+    category: "Video",
+    link: "https://youtu.be/pWB9zjUo694"
+  },
+
+  {
+    id: "08",
+    img: "/images/portofolio-08.jpg",
+    category: "Video",
+    link: "https://youtu.be/Q99_HITh2Kk"
+  },
+
+  {
+    id: "09",
+    img: "/images/portofolio-09.jpg",
+    category: "Video",
+    link: "https://www.instagram.com/reel/CeyPqovDcox/?utm_source=ig_web_copy_link"
+  },
+
+  {
+    id: "10",
+    img: "/images/portofolio-10.jpg",
+    category: "Video",
+    link: "https://youtu.be/cq_HMvndFJk"
+  },
+
+  {
+    id: "11",
+    img: "/images/portofolio-11.jpg",
+    category: "Video",
+    link: "https://www.youtube.com/watch?v=hN3kckLGAQo"
+  },
+
+  {
+    id: "12",
+    img: "/images/portofolio-12.jpg",
+    category: "Video",
+    link: "https://www.instagram.com/p/CUkvb9WDiz_/?utm_source=ig_web_copy_link"
+  },
+];
+
+export default portofolioData;
